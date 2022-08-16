3eb856088f9ced534bf335aaedbc8b1e6e8ed51f
Brad Teague <b_rad88@live.com>
2022-08-16 15:52:54 +0930

    Create notes route to get, post and delete

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,54 @@
 const router = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const { readDb, writeDb } = require("../utils/dbUtils");
 
-router.get("/", (req, res) => {});
+router.get("/", async (req, res, next) => {
+  try {
+    res.json(await readDb());
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post("/", (req, res) => {});
+router.post("/", async (req, res, next) => {
+  const { title, text } = req.body;
 
-router.delete("/", (req, res) => {});
+  if (!title || !text) {
+    return next();
+  }
+
+  try {
+    const id = uuidv4();
+    const data = await readDb();
+    data.push({ id, title, text });
+    const success = await writeDb(data);
+    if (success) {
+      return res.sendStatus(201);
+    }
+    res.sendStatus(500);
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const data = await readDb();
+    const reviewIdx = data.findIndex(review => review.id === id);
+    if (reviewIdx === -1) return res.sendStatus(400);
+    data.splice(reviewIdx, 1);
+    const success = await writeDb(data);
+    if (!success) {
+      return res.sendStatus(500);
+    }
+
+    res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
 
 module.exports = router;
